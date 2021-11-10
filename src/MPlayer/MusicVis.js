72a3aad0b4f795aa2ac9862d1fd0e22fc4a1a464
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-11-10 11:15:09 -0500

    MusicVis added yellow to visualizer

--- src/MPlayer/MusicVis.js
+++ src/MPlayer/MusicVis.js
@@ -4,7 +4,7 @@ export default function MusicPlayer(props) {
   return (
     <div>
       <svg>
-        <circle r={props.size} cx="100" cy="100" fill="#c26db4" />
+        <circle r={props.size} cx="100" cy="100" fill="#ebcd49" />
 
         <circle
           r={props.progress * props.size}
