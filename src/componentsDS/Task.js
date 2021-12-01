111dc1b750ec3367b1aa2ff46a73101c790a46b1
Stephanie Ranegar <83993810+sranegar@users.noreply.github.com>
2021-12-01 12:39:47 -0500

    update Tasks.js const useDrag added

--- src/componentsDS/Task.js
+++ src/componentsDS/Task.js
@@ -1,6 +1,15 @@
 import { Card } from "@mui/material";
+import { useDrag } from "react-dnd";
 
 export default function Task({ description, index, rowNum }) {
+  const [{ isDragging }, drag] = useDrag(
+    () => ({
+      type: "card",
+      item: { description: description, index: index, rowNum: rowNum }
+    }),
+    []
+  );
+
   return (
     <Card>
       <p>{description}</p>
